6ccfcb3a311a1cd5413b161c03a4d1c845bff2ab
danielmesa240493 <danielmesa240493@correo.itm.edu.co>
2021-08-30 12:45:53 -0500

    Add entity

---
+++ electronicwatches.Common/Models/Clock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace electronicwatches.Common.Models
+{
+    public class Clock
+    {
+        public DateTime Hour { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public int Type { get; set; }
+
+        public bool Consolidated { get; set; }
+    }
+}
---
+++ electronicwatches.Functions/Entities/ClockEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace electronicwatches.Functions.Entities
+{
+    public class ClockEntity : TableEntity
+    {
+        public DateTime Hour { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public int Type { get; set; }
+
+        public bool Consolidated { get; set; }
+    }
+}
