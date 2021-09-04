5ee4d55fabb022b87234209898e2e1783980edf7
danielmesa240493 <danielmesa240493@correo.itm.edu.co>
2021-09-04 18:12:01 -0500

    Get register by id unit test

--- electronicwatches.Test/Tests/ClockApiTest.cs
+++ electronicwatches.Test/Tests/ClockApiTest.cs
@@ -1,16 +1,18 @@
 ﻿using electronicwatches.Common.Models;
+using electronicwatches.Functions.Entities;
 using electronicwatches.Functions.Functions;
 using electronicwatches.Test.Helpers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
 using System;
 using Xunit;
 
 namespace electronicwatches.Test.Tests
 {
-    public class ClockApiTest
+    public class ClockApiTest : TableEntity
     {
         private readonly ILogger logger = TestFactory.CreateLogger();
 
@@ -41,6 +43,24 @@ namespace electronicwatches.Test.Tests
 
             //Act
             IActionResult response = await ClockApi.UpdateRegister(request, mockRegisters, registerId.ToString(), logger);
+
+            //Assert
+            OkObjectResult result = (OkObjectResult)response;
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+        }
+
+        [Fact]
+        public void GetRegisterById_Should_Return_200()
+        {
+            //Arrenge
+            MockCloudTableRegisters mockRegisters = new MockCloudTableRegisters(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            Clock clockRequest = TestFactory.GetRegisterRequest();
+            ClockEntity clockEntity = new ClockEntity();
+            Guid registerId = Guid.NewGuid();
+            DefaultHttpRequest request = TestFactory.CreateHttpRequest(registerId, clockRequest);
+
+            //Act
+            IActionResult response = ClockApi.GetRegisterById(request, clockEntity, registerId.ToString(), logger);
 
             //Assert
             OkObjectResult result = (OkObjectResult)response;
